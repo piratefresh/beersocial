31058acc9e29f0b161555767c449f6af9e765578
Magnus Nilsen <magnussithnilsen@gmail.com>
2019-05-28 11:52:15 -0400

    Fixed some beer resolvers

--- backend/src/resolvers/beer.js
+++ backend/src/resolvers/beer.js
@@ -1,23 +1,31 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
 export default {
-    Query: {
-        getBeer: async (_, {id}) => {
-            const response = await fetch(`https://sandbox-api.brewerydb.com/v2/beer/${id}?&key=c3422f64c2c852a1c1cb211ca5bcb65d`)
-            const data = await response.json()
-            console.log(data.data)
-            return data.data
-        },
-        getRandomBeer: async () => {
-            const response = await fetch(`https://sandbox-api.brewerydb.com/v2/beer/random?&key=c3422f64c2c852a1c1cb211ca5bcb65d`)
-            const data = await response.json()
-            console.log(data.data)
-            return data.data
-        }
+  Query: {
+    getBeer: async (_, {id}) => {
+      const response = await fetch(
+        `https://sandbox-api.brewerydb.com/v2/beer/${id}?&key=c3422f64c2c852a1c1cb211ca5bcb65d`
+      );
+      const data = await response.json();
+      console.log(data.data);
+      return data.data;
     },
-  };
-
-
-
-
-
+    getRandomBeer: async () => {
+      const response = await fetch(
+        `https://sandbox-api.brewerydb.com/v2/beers?&order=random&hasLabels=Y&availableId=1&withBreweries=Y&randomCount=1&key=c3422f64c2c852a1c1cb211ca5bcb65d`
+      );
+      const data = await response.json();
+      console.log(data.data);
+      return data.data;
+    },
+    // Get 10 Random beers for frontpage
+    getRandomBeers: async () => {
+      const response = await fetch(
+        `https://sandbox-api.brewerydb.com/v2/beers?&order=random&hasLabels=Y&availableId=1&withBreweries=Y&randomCount=9&key=c3422f64c2c852a1c1cb211ca5bcb65d`
+      );
+      const data = await response.json();
+      console.log(data.data);
+      return data.data;
+    }
+  }
+};
--- backend/src/schema/beer.js
+++ backend/src/schema/beer.js
@@ -1,45 +1,61 @@
+const gql = require("graphql-tag");
 
-const gql = require("graphql-tag")
-
-const beer = gql` 
-    extend type Query {
-        getBeer(id: String!): Beer
-        getRandomBeer: Beer
-        getBeers: [Beer]
-    }
-    type Beer {
-        id: ID!
-        name: String!
-        nameDisplay: String!
-        description: String
-        originalGravity: String
-        abv: String!
-        ibu: String
-        styleId: String!
-        style: Style!
-        status: String!
-        statusDisplay: String
-        availableId: Int
-        available: Available
-        beerVariationId: String
-        beerVariation: String
-        year: String!
-        isRetired: String
-    }
-    type Available {
-        id: ID
-        name: String
-        description: String
-    }
-    type Style {
-        id: ID
-        description: String!
-        name: String!
-        shortName: String!
-    }
-    type Category {
-        name: String
-    }
+const beer = gql`
+  extend type Query {
+    getBeer(id: String!): Beer
+    getRandomBeer: Beer
+    getRandomBeers: [Beer]
+  }
+  type Labels {
+    medium: String
+    contentAwareMedium: String
+    large: String
+    contentAwareLarge: String
+  }
+  type Available {
+    id: ID
+    name: String
+    description: String
+  }
+  type Style {
+    id: ID
+    description: String!
+    name: String!
+    shortName: String!
+  }
+  type Category {
+    name: String
+  }
+  type Brewery {
+    name: String!
+    website: String!
+    images: Images
+  }
+  type Images {
+    medium: String!
+    icon: String!
+  }
+  type Beer {
+    id: ID!
+    name: String!
+    nameDisplay: String!
+    description: String
+    originalGravity: String
+    abv: String!
+    ibu: String
+    styleId: String!
+    style: Style!
+    status: String!
+    statusDisplay: String
+    availableId: Int
+    available: Available
+    beerVariationId: String
+    beerVariation: String
+    year: String!
+    isRetired: String
+    labels: Labels!
+    breweries: [Brewery]!
+  }
 `;
 
 export default beer;
